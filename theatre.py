e8d9af3c8e31eaf9694cfd4eb4d2afeb6e939158
Anthony Gillard <anthony.g.gillard@gmail.com>
2024-03-31 18:11:38 +0100

    Added references directory and theatre class to host the simulation

---
+++ theatre.py
@@ -0,0 +1,9 @@
+class Theatre:
+    def start_show(self):
+        raise NotImplementedError()
+
+    def append_actor(self):
+        raise NotImplementedError()
+
+    def set_actors(self):
+        raise NotImplementedError()
